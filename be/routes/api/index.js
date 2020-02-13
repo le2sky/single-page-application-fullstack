806b729e38a4c676040a85d0b1e41bcf29a1e333
이하늘 <hee1149kr@naver.com>
2020-02-13 22:16:52 +0900

    SignIn/Out initial setting finished

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var createError = require('http-errors')
 
+
+//로그인을 할때는 토큰 검사가 필요없음.
+router.use('/sign', require('./sign'));
+
 router.all('*', (req,res,next) => {
-  //미들웨어 작성 부분
+  //미들웨어 작성 부분 -> 토큰 구현
   next();
 })
 
 router.use('/test', require('./test'));
 router.use('/user', require('./user'));
+
+
 
 router.all('*', function(req, res, next) {
   next(createError(404,'[WARNING] there is no api!'));

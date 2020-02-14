89f3d7606b8ba6565a8c6fd910851d9267cfc169
이하늘 <hee1149kr@naver.com>
2020-02-14 15:40:16 +0900

    Back : Completed implementation of token issuance

--- be/routes/api/sign/index.js
+++ be/routes/api/sign/index.js
@@ -2,6 +2,20 @@ var express = require('express')
 var createError = require('http-errors')
 var router = express.Router();
 
+const jwt = require('jsonwebtoken')
+const cfg = require('../../../../config/inedx')
+const User = require('../../../models/users')
+
+
+
+const signToken = (id, name) => {
+    return new Promise((resolve, reject) => {
+        jwt.sign({id, name}, cfg.secretKey , (err, token)=>{
+            if (err) reject(err)
+            resolve(token)
+        })
+    })
+}
 
 //post 방식의 호출의 두번째 인자는 => body
 //get 방식의 호출의 두번째 인자는 => option -> request header 같은거
@@ -16,13 +30,16 @@ router.post('/in', (req,res,next) => {
         success: false,
         msg: '비밀번호를 입력해 주세요'
     });
-    
-    console.log(`${id}님이 접속하셨습니다.`)
-    
-    res.send({
-        success: true,
-        token: 'temp token'
-    });
+
+    User.findOne({id}).then((r)=>{
+        if(!r) throw new Error('존재하지 않는 아이디입니다.')
+        if(r.pwd !== pwd) throw new Error('비밀번호가 틀립니다.')
+        return signToken(r.id, r.name)
+    }).then((r) => {
+        res.send({success: true, token: r})
+    }).catch((e) => {
+        res.send({success: false, msg: e.message})
+    })
 });
 
 router.post('./out', (req,res,next) => {
